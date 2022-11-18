7b1b2058cf5f3f7d22017d5f89d46c78241f69f6
Adi3220 <117853273+Adi3220@users.noreply.github.com>
2022-11-18 20:02:36 +0530

    Update reversingthearray1.cpp
    code for reversing the array

--- reversingthearray1.cpp
+++ reversingthearray1.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
-
 using namespace std;
+
 int main()
 {
     int arr[6]={10,54,78,5,45,7};
     int n=6;
     
-        for(int i=0; i<n; i=i+2){
-            swap(arr[i], arr[n-1-i]);
-        }
+    for(int i=0; i<n; i=i+2){
+        swap(arr[i], arr[n-1-i]);
+    }
     for(int j=0; j<n; j++){
         cout<< arr[j]<<" ";
     }
-      
-    
-
     return 0;
 }
